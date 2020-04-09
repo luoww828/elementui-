2e5804c5b68c1aa589cea5f0508ce7c385331f5b
Luo Wen Wei <Luo.Wen.Wei@esl.epson.com.hk>
2020-04-09 17:03:07 +0800

    elementui change theme color

---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <el-menu class="navbar" mode="horizontal">
+    <hamburger 
+      class="hamburger-container"
+      :toggleClick="toggleSideBar"
+      :isActive="!sidebar.opened">
+    </hamburger>
+    <div class="right-menu">
+      <screenfull class="screenfull"></screenfull>
+      <div class="lang">
+        <el-dropdown>
+          <i class="iconfont icon-language4"></i>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item @click.native="toggleLang('zh')" :disabled="$i18n.locale == 'zh'">中文</el-dropdown-item>
+            <el-dropdown-item @click.native="toggleLang('en')" :disabled="$i18n.locale == 'en'">English</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </div>
+      <theme-picker></theme-picker>
+    </div>
+  </el-menu>
+</template>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import Element from 'element-ui'
+import App from './App.vue'
+import '../theme/index.css'
+
+Vue.config.productionTip = false
+
+Vue.use(Element)
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
